d9d0a11df254632c62204a6875c9258213d8a9cf
Joseph Kopecky <joseph.m.kopecky@gmail.com>
2024-08-08 18:07:56 -0500

    Defined constant fonts and colors.

--- src/main/java/dev/prognitio/MathMindsWebsite/HomeController.java
+++ src/main/java/dev/prognitio/MathMindsWebsite/HomeController.java
@@ -1,13 +1,15 @@
 package dev.prognitio.MathMindsWebsite;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "Hello World";
+    public String home(Model model) {
+        return "home";
     }
 }
