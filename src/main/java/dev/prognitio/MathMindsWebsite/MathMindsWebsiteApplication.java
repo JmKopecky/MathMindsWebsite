a822679ad8aa92cd86dddaf0dd636d1cf3ec6612
Joseph Kopecky <joseph.m.kopecky@gmail.com>
2024-08-08 17:30:00 -0500

    Initial commit

---
+++ src/main/java/dev/prognitio/MathMindsWebsite/MathMindsWebsiteApplication.java
@@ -0,0 +1,13 @@
+package dev.prognitio.MathMindsWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathMindsWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathMindsWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/prognitio/MathMindsWebsite/MathMindsWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.prognitio.MathMindsWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathMindsWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
